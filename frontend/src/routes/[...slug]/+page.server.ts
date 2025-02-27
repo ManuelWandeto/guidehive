5bae086800effa1841c903d59e39cd388e3cd431
Manuel Wandeto <46853664+ManuelWandeto@users.noreply.github.com>
2025-02-27 23:27:13 +0300

    redirect to first page within the project

--- frontend/src/routes/[...slug]/+page.server.ts
+++ frontend/src/routes/[...slug]/+page.server.ts
@@ -2,33 +2,31 @@ import type { PageApiResponse, SectionApiResponse } from "$lib/interfaces/doc";
 import type { PageServerLoad } from "./$types";
 import { error, redirect } from '@sveltejs/kit';
 import {APP_ENV, DEV_API_URL, PROD_API_URL} from '$env/static/private';
+import {PUBLIC_BASE_URL} from '$env/static/public';
 
 
-export const load: PageServerLoad = async ({fetch, params, parent}) => {
+export const load: PageServerLoad = async ({fetch, params, parent, url}) => {
     const api_url = APP_ENV === 'development' ? DEV_API_URL : PROD_API_URL;
 
     if(!params.slug?.trim()) {
         const {data} = await parent()
-        throw redirect(308, `./${data[0].slug}`)
+        const redirectUrl = `${url.origin + PUBLIC_BASE_URL}/${data[0].slug}`
+        
+        throw redirect(307, redirectUrl)
     }
-    try {
-        const slug = params.slug?.split('/').pop()
-        // try find a section first
-        const sectionRes = await fetch(`${api_url}/api/sections?filters[slug][$eq]=${slug}`)
-        const section: SectionApiResponse = await sectionRes.json()
-        if(section.data.length) {
-            return section;
-        }
-        // if not find a page
-        const pageRes = await fetch(`http://localhost:1337/api/pages?filters[slug][$eq]=${slug}`)
-        const page:PageApiResponse = await pageRes.json();
+    const slug = params.slug?.split('/').pop()
+    // try find a section first
+    const sectionRes = await fetch(`${api_url}/api/sections?filters[slug][$eq]=${slug}`)
+    const section: SectionApiResponse = await sectionRes.json()
+    if(section.data.length) {
+        return section;
+    }
+    // if not find a page
+    const pageRes = await fetch(`${api_url}/api/pages?filters[slug][$eq]=${slug}`)
+    const page:PageApiResponse = await pageRes.json();
 
-        if(page.data.length) {
-            return page;
-        }
-        error(404, 'NOT FOUND')
-    } catch (e) {
-        console.log(e)
-        error(500, `Internal error getting page: ${e}`)
+    if(page.data.length) {
+        return page;
     }
+    error(404, 'NOT FOUND')
 }
