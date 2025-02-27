eec686936652f84da975f1be89a0c02ffe45ff0a
Manuel Wandeto <46853664+ManuelWandeto@users.noreply.github.com>
2025-02-27 23:27:35 +0300

    show project name in the navbar

--- frontend/src/routes/[...slug]/+page.svelte
+++ frontend/src/routes/[...slug]/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import MarkdownRenderer from '$lib/components/markdown_renderer.svelte';
-    import type {PageProps} from './$types'
-    const {data}: PageProps = $props();
+    import type {PageProps} from './$types';
+    import {PUBLIC_PROJECT} from '$env/static/public';
 
+    const {data}: PageProps = $props();
 </script>
 
 <svelte:head>
-	<title>Hotelex - {data.data[0].title}</title>
+	<title>{PUBLIC_PROJECT} | {data.data[0].title}</title>
 </svelte:head>
 
 <MarkdownRenderer mdContent = {data.data[0].content}/>
